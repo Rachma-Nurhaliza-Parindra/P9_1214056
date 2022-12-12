6fd360ae68116b6590cf5bcfef6a1dc2ea7b4ecf
Rachma <rachmanurhalizastudent@gmail.com>
2022-12-12 15:41:11 +0700

    Add project files.

---
+++ P9_1_1214056/controller/Koneksi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace P9_1_1214056
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=ulbi;Uid=root;Pwd=;";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionstring);
+            kon.Open();
+        }
+        
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            MySqlCommand command = new MySqlCommand(query, kon);
+            command.ExecuteNonQuery();
+        }
+
+        public object ShowData(string query)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connectionstring);
+            DataSet data = new DataSet();
+
+            adapter.Fill(data);
+            object datable = data.Tables[0];
+            return datable;
+        }
+    }
+}
