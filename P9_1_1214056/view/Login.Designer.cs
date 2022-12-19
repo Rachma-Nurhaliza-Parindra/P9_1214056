659a9ac368a2997fcb539415a7225b91ad571537
Rachma <rachmanurhalizastudent@gmail.com>
2022-12-19 08:00:39 +0700

    New

--- P9_1_1214056/view/Login.Designer.cs
+++ P9_1_1214056/view/Login.Designer.cs
@@ -86,6 +86,7 @@
             // 
             this.tbPassword.Location = new System.Drawing.Point(111, 93);
             this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(150, 20);
             this.tbPassword.TabIndex = 5;
             // 
